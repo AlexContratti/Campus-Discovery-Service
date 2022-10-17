8b19f15180602aeef6662b91be097f6dd0631079
panya <panya@future.green>
2022-10-17 13:34:46 -0400

    Styled config screen

--- campus_discovery_project/src/pages/Config.js
+++ campus_discovery_project/src/pages/Config.js
@@ -1,46 +1,45 @@
 import React from "react"
 import {Link } from "react-router-dom";
+import './Config.css';
 
 
 function Config() {
     return (
         <div className="config-container">
             <div>
-                <Link to="/"><button> Exit </button> </Link>
+                <Link to="/"> <button className="exit-button"> Exit </button> </Link>
             </div>
-            <div className = "App">
-                <header> Initial Configuration Screen </header>
-                <br></br><br></br>
+            <div className = "input-container">
+                <div className="header"> Create New Account </div>
                 <form>
+
+                    <div className="label"> Name: </div>
                     <label>
-                        Name:
                         <input type = "text" name = "name"></input>
                     </label>
-                    <br></br>
+                    
+                    <div className="label"> Who are you: </div>
                     <label>
-                        Who are you:
-                        <select>
+                        <select className="select">
                             <option value = "Student">Student</option>
                             <option value = "Teacher">Teacher</option>
                             <option value = "Organizer">Organizer</option>
                         </select>
                     </label>
-                    <br></br><br></br>
+                    
+                    <div className="label"> Username: </div>
                     <label>
-                        Username:
                         <input type = "text" name = "username"></input>
                     </label>
-                    <br></br>
+                   
+                    <div className="label"> Password: </div>
                     <label>
-                        Password:
                         <input type = "text" name = "password"></input>
                     </label>
-                    <br></br><br></br>
-                    <Link to="/Events"><button>
-                        Submit
-                    </button>
-                    </Link>
                 </form>
+                   
+                <Link to="/Events"><button className="submit"> Submit </button> </Link>
+                
             </div>
         </div>
     );
--- campus_discovery_project/src/pages/Login.js
+++ campus_discovery_project/src/pages/Login.js
@@ -11,19 +11,22 @@ function Login() {
             </div>
             <div className = "input-container">
                 <div className="header"> Login </div>
-                <div className="label"> Username: </div>
-                <label className="input">
-                    <input type = "text" name = "username"></input>
-                </label>
-                <div className="label"> Password: </div>
-                <label className="input">
-                    <input type = "text" name = "password"></input>
-                </label>
-                    
-                <Link to="/Events"><button className="login-button">
-                    Submit
-                    </button>
-                </Link>
+                <form>
+                    <div className="label"> Username: </div>
+                    <label className="input">
+                        <input type = "text" name = "username"></input>
+                    </label>
+                    <div className="label"> Password: </div>
+                    <label className="input">
+                        <input type = "text" name = "password"></input>
+                    </label>
+                </form>
+
+                    <Link to="/Events"><button className="login-button">
+                        Submit
+                        </button>
+                    </Link>
+                
             </div>
         </div>
     );
