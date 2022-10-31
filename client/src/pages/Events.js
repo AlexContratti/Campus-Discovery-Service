1961a4c3b0122a8883eb977a6de4a55c03d29382
panya <pgbhinder@gmail.com>
2022-10-31 14:08:39 -0400

    add button created on events page

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -2,6 +2,8 @@ import React from "react"
 import {Link } from "react-router-dom";
 import Sidebar from '../components/Sidebar';
 import './Events.css';
+import AddIcon from '@mui/icons-material/Add';
+
 
 
 function Events() {
@@ -10,6 +12,10 @@ function Events() {
             <div className="nav-bar"><Sidebar/></div>
             <div className = "events">
                 <div className="pageheader"> Events </div>
+                <div className="tool-bar">
+                    <div className="add-button"> <AddIcon/> Add Event </div>
+                    {/*<div className="delete-button" onClick={() => setShowModal(true)}>Delete</div>*/}
+                </div>
             </div>
         </div>
     );
