baccd3e3f6d0a4d63a8dd787be224bea4051d5b3
panya <pgbhinder@gmail.com>
2022-11-02 19:31:34 -0400

    fixed database and login auth endpoints

--- server/connect_to_mongo.js
+++ server/connect_to_mongo.js
@@ -1,91 +1,107 @@
-const { MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-async function main() {
-    const uri = "mongodb+srv://AkashM12:MongoMaster69@cluster0.etwxzme.mongodb.net/?retryWrites=true&w=majority";
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+class Database {
+    constructor() {
 
-    try {
-        await client.connect();
+    }
 
-        //CALL ALL THE METHODS HERE IN MAIN WITH await <method>
-        //FOR THE PARAMS U PASS IN CLIENT AND THE PARAMS IN JSON FORMAT (If its single param like a name just do a string)
+    async connect() {
+        const uri = "mongodb+srv://AkashM12:MongoMaster69@cluster0.etwxzme.mongodb.net/?retryWrites=true&w=majority";
+        this.client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    } catch (e) {
-        console.error(e);
+        try {
+            await this.client.connect();
 
-    } finally {
-        await client.close();
+            //CALL ALL THE METHODS HERE IN MAIN WITH await <method>
+            //FOR THE PARAMS U PASS IN CLIENT AND THE PARAMS IN JSON FORMAT (If its single param like a name just do a string)
 
+        } catch (e) {
+            console.error(e);
+
+        } finally {
+            // await this.client.close();
+        }
+    }
+
+    async createEvent(newEvent) {
+        const result = await this.client.db("campus_discovery").collection("events").insertOne(newEvent);
+
+        console.log(`New event created with the following id: ${result.insertedId}`);
+    }
+
+    async createUser(newUser) {
+        const result = await this.client.db("campus_discovery").collection("users").insertOne(newUser);
+
+        console.log(`New user created with the following id: ${result.insertedId}`);
+    }
+
+    async getLogin({
+        username = "user",
+        password = "pass"
+    } = {}) {
+        const result = await this.client.db("campus_discovery").collection("users").findOne(
+            {
+                username: username,
+                password: password
+            });
+
+        if (result) {
+            console.log(`Found a user in the collection with the username ${username} or password ${password}:`);
+            console.log(result);
+            return true;
+        } else {
+            console.log(`No users found with the username ${username} or password ${password}`);
+            return false;
+        }
+    }
+
+    async getEvents(eventName) {
+        const cursor = this.client.db("campus_discovery").collection("events").find(
+            {name: eventName})
+            .sort({ time: -1 });
+
+        const results = await cursor.toArray();
+
+        if (results.length > 0) {
+            console.log(`events found with name ${eventName}`);
+        } else {
+            console.log(`no events found with name ${eventName}`);
+        }
+    }
+
+    async getUserInfo({
+        username = "user",
+        password = "pass"
+    } = {}) {
+        const result = await this.client.db("campus_discovery").collection("users").findOne(
+            {
+                username: username,
+                password: password
+            }, {name: 1, type: 1});
+
+        if (result) {
+            console.log(`Found a user's info in the collection with the username ${username} or password ${password}:`);
+            console.log(result);
+        } else {
+            console.log(`No user's info found with the username ${username} or password ${password}`);
+        }
+    }
+
+    async deleteEvent(eventName) {
+        const result = await this.client.db("campus_discovery").collection("events")
+                .deleteOne({ name: eventName });
+
+        console.log(`${result.deletedCount} document(s) was/were deleted.`);
     }
 }
 
-main().catch(console.error);
+module.exports = { Database };
 
-async function createEvent(client, newEvent) {
-    const result = await client.db("campus_discovery").collection("events").insertOne(newEvent);
+// let db;
 
-    console.log(`New event created with the following id: ${result.insertedId}`);
-}
-
-async function createUser(client, newUser) {
-    const result = await client.db("campus_discovery").collection("users").insertOne(newUser);
-
-    console.log(`New event created with the following id: ${result.insertedId}`);
-}
-
-async function getLogin(client, {
-    username = "user",
-    password = "pass"
-} = {}) {
-    const result = await client.db("campus_discovery").collection("users").findOne(
-        {
-            username: username,
-            password: password
-        });
-
-    if (result) {
-        console.log(`Found a user in the collection with the username ${username} or password ${password}:`);
-        console.log(result);
-    } else {
-        console.log(`No users found with the username ${username} or password ${password}`);
-    }
-}
-
-async function getEvents(client, eventName) {
-    const cursor = client.db("campus_discovery").collection("events").find(
-        {name: eventName})
-        .sort({ time: -1 });
-
-    const results = await cursor.toArray();
-
-    if (results.length > 0) {
-        console.log(`events found with name ${eventName}`);
-    } else {
-        console.log(`no events found with name ${eventName}`);
-    }
-}
-
-async function getUserInfo(client, {
-    username = "user",
-    password = "pass"
-} = {}) {
-    const result = await client.db("campus_discovery").collection("users").findOne(
-        {
-            username: username,
-            password: password
-        }, {name: 1, type: 1});
-
-    if (result) {
-        console.log(`Found a user's info in the collection with the username ${username} or password ${password}:`);
-        console.log(result);
-    } else {
-        console.log(`No user's info found with the username ${username} or password ${password}`);
-    }
-}
-
-async function deleteEvent(client, eventName) {
-    const result = await client.db("campus_discovery").collection("events")
-            .deleteOne({ name: eventName });
-
-    console.log(`${result.deletedCount} document(s) was/were deleted.`);
-}
+// (async function() {
+//     db = new Database();
+//     await db.connect();
+//     console.log(await db.client.db("campus_discovery").collection("users").count())
+//     setTimeout(db.getUserInfo, 5000);
+// })()
--- server/index.js
+++ server/index.js
@@ -1,20 +1,29 @@
 const express = require("express");
 var cors = require("cors");
 var bodyParser = require('body-parser');
+const { Database } = require('./connect_to_mongo');
+
+let db = new Database();
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 var jsonBodyParser = bodyParser.json({type: "application/json"});
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.post("/register", jsonBodyParser, async (req, res) => {
+app.post("/register", async (req, res) => {
   try {
     console.log("/register POST Request Received");
     var {name, username, password, type} = req.body;
-
     console.log(name + " " + username + " " + password + " " + type);
+    if (!name || !username || !password || !type) {
+      res.status(400).send("Supply name, username, password, and type");
+      return;
+    }
+    await db.createUser(req.body);
     res.send("Validated");
     res.status(201);
   } catch (err) {
@@ -27,16 +36,25 @@ app.post("/login", jsonBodyParser, async (req, res) => {
   try {
     console.log("/login POST Request Received");
     var {username, password} = req.body;
-
+    if (!username || !password) {
+      res.status(400).send("Supply username and password");
+      return;
+    }
+    let validated = await db.getLogin(req.body);
     console.log(username + " " + password);
-    res.send("Validated");
-    res.status(201)
+    if (validated) {
+      res.status(200).send("Validated");
+    } else {
+      res.status(403).send("Invalid credentials");
+    }
+
   } catch (err) {
     console.log(err);
     res.status(400);
   }
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+  await db.connect();
   console.log(`Server listening on ${PORT}`);
 });
